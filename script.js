ee6992e535f7b186fe589ee55cbb3af50d995447
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-09-20 23:02:26 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+const heading = document.getElementById("title");
+heading.style.fontSize = "100px";
+
+const h2 = document.createElement("h2");
+h2.textContent = "This content is brought to you by JS!";
+document.querySelector("body").appendChild(h2);
